cef9568f3f86ffbc99a1c7a970a5cd424162cb20
liujuntao <ljt1018168@163.com>
2017-05-18 14:38:33 +0800

    finish static pages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+get('/', 'StaticPagesController@home');
+get('/help', 'StaticPagesController@help');
+get('/about', 'StaticPagesController@about');
