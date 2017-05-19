717803c7a8796d68e9dc9a65217ddd4e0506320f
liujuntao <ljt1018168@163.com>
2017-05-19 16:50:38 +0800

    finish layout and route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,7 @@
 //     return view('welcome');
 // });
 
-get('/', 'StaticPagesController@home');
-get('/help', 'StaticPagesController@help');
-get('/about', 'StaticPagesController@about');
+get('/', 'StaticPagesController@home')->name('home');
+get('/help', 'StaticPagesController@help')->name('help');
+get('/about', 'StaticPagesController@about')->name('about');
+get('signup', 'UsersController@create')->name('signup');
