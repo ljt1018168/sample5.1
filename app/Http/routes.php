d7d820f36f98d9bb0c29199759b61439ef83f8fe
liujuntao <ljt1018168@163.com>
2017-05-24 08:33:41 +0000

    finish user signup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,21 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 get('/', 'StaticPagesController@home')->name('home');
 get('/help', 'StaticPagesController@help')->name('help');
 get('/about', 'StaticPagesController@about')->name('about');
+
 get('signup', 'UsersController@create')->name('signup');
+resource('users', 'UsersController');
